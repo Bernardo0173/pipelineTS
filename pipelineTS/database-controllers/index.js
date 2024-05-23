0c152730386bc4d2bb3c3533f9c8ff36f70ca026
Bernardo0173 <102173762+Bernardo0173@users.noreply.github.com>
2024-05-23 14:40:31 -0600

    Cambio del puerto
    Cambiamos el puerto de 3000 a 8085

--- pipelineTS/database-controllers/index.js
+++ pipelineTS/database-controllers/index.js
@@ -6,7 +6,7 @@ const mysqlController = require('./mysqlController');
 const dynamoController = require('./dynamoController');
 
 const app = express();
-const port = 3000;
+const port = 8085;
 
 app.use(bodyParser.json());
 
